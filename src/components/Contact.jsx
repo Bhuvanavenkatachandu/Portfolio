d2f2e0d7b86179694dcd9eb92f56a45365c0fa22
Bhuvana Venkata Chandu <eluribhuvanavenkatachandu@gmail.com>
2025-07-17 18:11:04 +0530

    mail message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,12 +26,12 @@ const Contact = () => {
         </a>
       </p>
 
-      <a
-        href="mailto:eluribhuvanavenkatachandu@gmail.com?subject=Let's%20Connect&body=Hi%20Bhuvanavenkatachandu%2C%0A%0AI'd%20like%20to%20reach%20out%20regarding..."
-        className="message-button"
-      >
-        Send Message
-      </a>
+     <a
+    href="mailto:eluribhuvanavenkatachandu@gmail.com?subject=Let's%20Connect&body=Hi%20Bhuvanavenkatachandu%2C%0A%0AI%20came%20across%20your%20portfolio%20and%20would%20love%20to%20connect%20with%20you.%20Please%20let%20me%20know%20a%20good%20time%20to%20talk.%0A%0ALooking%20forward%20to%20hearing%20from%20you.%0A%0ABest%20regards%2C%0A[Your%20Name]"
+    className="message-button"
+  >
+    Send Message
+  </a>
     </section>
   );
 };
